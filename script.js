01ce24016f8fbad8ac306381b68e8f157281b32a
Ninoslav Zebic <zebicn@gmail.com>
2019-10-09 22:28:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,24 +3,16 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
-console.log(body);
-
-
-color1.addEventListener("input", function(){
+function setGradient() {
 	body.style.background = "linear-gradient(to right, " 
-	+ color1.value
+	 + color1.value
 	 + ", " 
 	 + color2.value 
 	 + ")"; 
 
-	css.textContent = body.style.background + ";";
-})
+	 css.textContent = body.style.background + ";";
+}
 
-color2.addEventListener("input", function(){
-	body.style.background = "linear-gradient(to right, " 
-	+ color1.value
-	 + ", " 
-	 + color2.value 
-	 + ")"; 
-
-})
+color1.addEventListener("input", setGradient);
+	
+color2.addEventListener("input", setGradient);
